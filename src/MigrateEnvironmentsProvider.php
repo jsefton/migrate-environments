0b34ef130a4bc4f324172575ed3770d1ea5710d9
Jamie Sefton <jamie.sefton@parall.ax>
2017-08-01 20:17:29 +0100

    Added initial service provider

---
+++ src/MigrateEnvironmentsProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jsefton\MigrateEnvironments;
+
+use Illuminate\Support\ServiceProvider;
+
+class MigrateEnvironmentsProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
